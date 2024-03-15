7a1be4f45fe535efdfb0adeef5533885c95d11f2
Samir <samirhassen14@gmail.com>
2024-03-15 13:30:03 -0500

    simplified code a bit and also moved argparser to utils.py

--- train.py
+++ train.py
@@ -7,7 +7,6 @@ import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
 
-
 # globals
 start_epoch = 1
 best_val_loss = sys.float_info.max
@@ -20,83 +19,28 @@ pt_path = utils.get_file_path(config.param_dir, config.pt_file)
 
 # command line interface argument parser
 parser = argparse.ArgumentParser(description="PyTorch Training")
-parser.add_argument(
-    "-lr",
-    "--learning_rate",
-    default=config.learning_rate,
-    type=float,
-    help="optimizer's initial learning rate",
-    dest="lr",
-)
-parser.add_argument(
-    "-pt",
-    "--pt_path",
-    default=pt_path,
-    type=str,
-    help="absolute path to the saved pytorch model",
-    dest="pt",
-)
-
-args = parser.parse_args()
+args = utils.train_arg_parser(parser, config, pt_path)
 print(args)
 
-if config.initialize == "start":
-    print("Starting a new model")
-    # Create transforms for preprocessing images to input into models
-    transform_train, transform_val = utils.get_transforms(
-        config.model_name, config.dataset_name
-    )
-
-    # Create dataset for both training and validation
-    train_dataset, val_dataset = utils.get_datasets(data_dir, config.dataset_name)
-    train_dataset.transform = transform_train
-    val_dataset.transform = transform_val
-
-    # model
-    model = utils.get_model(config.model_name, train_dataset, config)
-    model.to(config.device_type)
-
-    # optimizer
-    # TODO: write a get_optimizer function in utils? to experiment which ones work well?
-    optimizer = torch.optim.AdamW(model.parameters(), lr=args.lr)
-    # lr_scheduler = torch.optim.lr_scheduler.StepLR(
-    #     optimizer, step_size=config.step_size, gamma=config.gamma
-    # )
-elif config.initialize == "resume":
+# TODO: probably should just be a boolean. What else would there be besides starting and resuming?
+if args.init == "resume":
     print("Resuming a model")
     torch_model = torch.load(pt_path)
     config = torch_model["config"]
     start_epoch = torch_model["epoch"] + 1
     best_val_loss = torch_model["best_val_loss"]
-
-    # Create transforms for preprocessing images to input into models
-    transform_train, transform_val = utils.get_transforms(
-        config.model_name, config.dataset_name
-    )
-
-    # Create dataset for both training and validation
-    train_dataset, val_dataset = utils.get_datasets(data_dir, config.dataset_name)
-    train_dataset.transform = transform_train
-    val_dataset.transform = transform_val
-
-    # model
-    model = utils.get_model(config.model_name, train_dataset, config)
-    model.to(config.device_type)
-    model.load_state_dict(torch_model["model"])
-
-    # optimizer
-    # TODO: possibly overwrite lr, step_size, gamma, etc. in the config object then create the optimizer outside of this if else?
-    optimizer = torch.optim.AdamW(model.parameters(), lr=args.lr)
-    optimizer.load_state_dict(torch_model["optimizer"])
-    for param_group in optimizer.param_groups:
-        param_group["lr"] = args.lr
-    # lr_scheduler = torch.optim.lr_scheduler.StepLR(
-    #     optimizer, step_size=config.step_size, gamma=config.gamma
-    # )
-    # lr_scheduler = torch_model["lr_scheduler"]
 else:
-    raise Exception("Initialize value is incorrect. Use 'start' or 'resume'")
+    print("Starting a new model")
 
+# Create transforms for preprocessing images to input into models
+transform_train, transform_val = utils.get_transforms(
+    config.model_name, config.dataset_name
+)
+
+# Create dataset for both training and validation
+train_dataset, val_dataset = utils.get_datasets(data_dir, config.dataset_name)
+train_dataset.transform = transform_train
+val_dataset.transform = transform_val
 
 # dataloaders
 train_loader = DataLoader(
@@ -106,8 +50,23 @@ val_loader = DataLoader(
     val_dataset, batch_size=config.batch_size, shuffle=False, num_workers=4
 )
 
+# model
+model = utils.get_model(config.model_name, train_dataset, config)
+model.load_state_dict(torch_model["model"]) if args.init == "resume" else None
+model.to(config.device_type)
+
+# optimizer
+# TODO: possibly overwrite lr, step_size, gamma, etc. in the config object then create the optimizer outside of this if else?
+optimizer = torch.optim.AdamW(model.parameters(), lr=args.lr)
+optimizer.load_state_dict(torch_model["optimizer"]) if args.init == "resume" else None
+for param_group in optimizer.param_groups:
+    param_group["lr"] = args.lr
+
 # loss criterion
 criterion = nn.CrossEntropyLoss().to(config.device_type)
+
+print("Starting Best Val Loss: {}".format(best_val_loss))
+print(optimizer.param_groups[0]["lr"])
 
 # training
 for epoch in range(start_epoch, config.epochs + 1):
